de2aee37172427b72f9c523dff85e2bdf141642d
tibo35 <54692607+tibo35@users.noreply.github.com>
2021-12-12 13:36:41 +1000

    finished the features

--- text.js
+++ text.js
@@ -1,5 +1,5 @@
 let toDos = [];
-let completedToDos = [];
+
 
 const input = document.querySelector('#inputBox')
 const form = document.querySelector('.toDoForm')
@@ -7,52 +7,182 @@ const form = document.querySelector('.toDoForm')
 const toDoList = document.querySelector('.toDoList')
 const completedList = document.querySelector('.completedList')
 
-form.addEventListener('submit', checkInput = (event) => {
 
+// event submit => add userinput to array toDos
+
+form.addEventListener('submit', (event)=> {
     event.preventDefault()
     let userInput = input.value
-  if(userInput){
-      toDos.push(userInput)
-      addToList()
-  }
-userInput.value = ''
-})
+    if(userInput){
+        toDos.push(userInput)
+        addToDoList()
+    }
 
+});
+// loop through elements toDos
 
-
-function addToList(){
+function addToDoList (){
     toDoList.innerHTML = ''
-for (let i = 0; i < toDos.length; i++){
-show(toDos[i])
+    for (let i = 0; i < toDos.length; i++){
+        showToDos(toDos[i])
+    document.querySelector("#toDoCount").innerText = toDos.length;
 }
-document.querySelector("#toDoCount").innerText = toDos.length;
 }
+//show elements of toDos
 
-function show(userInput){
+function showToDos(test){
+
 const li = document.createElement('li');
-
-li.classList.add('toDoItem');   
-li.innerText = userInput
+li.classList.add('toDoItem')
+li.innerText = test
 toDoList.appendChild(li);
+
+// remove toDo item li
+
+li.addEventListener('click', removeToDoItem)
+
+
+function removeToDoItem(){
+    let index = toDos.indexOf(li)
+    toDos.splice(index, 1)
+    toDoList.removeChild(li)
+    completed.push(li)
+    addCompletedList()
+    document.querySelector("#toDoCount").innerText = toDos.length;
+
 }
 
-const toDoItem = document.querySelector('.toDoItem')
-toDoItem.addEventListener('click', removeToDo)
+}
+let completed = []
+// // loop through elements completed
 
-function removeToDo(){
-    toDoItem.remove()
+function addCompletedList (){
+    completedList.innerHTML = ''
+    for (let i = 0; i < completed.length; i++){
+        showCompleted(completed[i].innerText)
+        console.log(completed[i].innerText)
+        document.querySelector("#completedToDoCount").innerText = completed.length;
+ 
+}
+}
+// //show elements of completed
+function showCompleted(test){
+
+    const li = document.createElement('li');
+    li.classList.add('doneItem')
+    li.innerText = test
+    completedList.appendChild(li);
 }
 
 
-// 1- quand l'user click sur tdl item
 
-// 2- retirer to-do de la list et de l'array
 
-//3 - ajouter to do a la completed to-dos array refaire la completed liste
 
-//4 faire le counter pour la tdl
 
-// 5 faire le conter pour la completed list
+
+
+
+
+
+
+
+
+
+
+// form.addEventListener('submit', checkUserInput)
+
+// function checkUserInput (){
+//     event.preventDefault()
+//     let userInput = input.value
+//   if(userInput){
+//       toDos.push(userInput)
+//       addToList()
+//   }
+// userInput.value = ''
+// }
+
+
+
+// function addToList(){
+//     toDoList.innerHTML = ''
+// for (let i = 0; i < toDos.length; i++){
+// show(toDos[i])
+// }
+// // document.querySelector("#toDoCount").innerText = toDos.length;
+// }
+
+// function show(userInput){
+// const li = document.createElement('li');
+
+// li.classList.add('toDoItem')
+
+
+// li.innerText = userInput
+// toDoList.appendChild(li);
+
+// li.addEventListener('click', removeItem)
+// function removeItem(){
+//     toDoList.removeChild(li)
+    
+//     completed.push(li)
+
+//     addToCompletedList()
+// }
+// function addToCompletedList(){
+//     completedList.innerHTML=''
+// for (let i = 0; i < completed.length; i++){
+//     showCompleted(completed[i])
+// }
+// }
+
+
+// function showCompleted(completed){
+//     const li = document.createElement('li');
+
+// li.classList.add('doneItem')
+// li.innerText = completed
+// completedList.appendChild(li);
+// }
+// }
+
+
+// // When the user clicks a to-dolist item
+
+// // - Remove that to-do from the to-do array and list
+// toDoList.removeChild(item[i])
+// toDos.pop(item[i])
+
+
+// //3 - ajouter to do a la completed to-dos array refaire la completed liste
+// let done = [];
+// done.push(item[i])
+// addToCompleteList(done)
+// })
+// }
+
+// // - Add that to-do to a completed to-dos array and render that completed list 
+
+//  function addToCompleteList(){
+//     completedList.innerHTML = ''
+//     for(let i = 0; i<done.length; i++){
+//         showCompleteList(done[i])
+//     }
+    
+//  }
+// function showCompleteList(item){
+   
+
+//     li.classList.add('completedList');   
+//     li.innerText = done
+//     completedList.appendChild(li);  
+// }
+// }
+
+
+
+
+
+
 
 
 
